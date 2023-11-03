0e99ef12b6720005a2b54c5e66de3798319d1756
전아름 <j2665138@naver.com>
2023-11-03 15:42:25 +0900

    테스트 코드 작성

--- testJUnit/src/test/java/com/example/testjunit/AnnotationTest.java
+++ testJUnit/src/test/java/com/example/testjunit/AnnotationTest.java
@@ -10,30 +10,33 @@ import org.junit.jupiter.api.DisplayName;
 @Slf4j
 public class AnnotationTest {
 
+    @Test
     @DisplayName("JUnit 기본 Annotation Test")
-    @org.junit.Test
-    public void JunitAnnotationTest() {
-
+    public void JunitAnnotationTest1() {
+        log.info("@DisplayName 확인");
 
     }
 
-    @DisplayName("Timeout 에러 테스트")
+
     @Test(timeout = 1)
+    @DisplayName("Timeout 에러 테스트")
     public void JunitAnnotationTest2() throws Exception {
         Thread.sleep(100);
 
     }
 
-    @DisplayName("RuntimeException 오류 예상 테스트")
+
     @Test(expected = RuntimeException.class)
+    @DisplayName("RuntimeException 오류 예상 테스트")
     public void JunitAnnotationTest3() throws Exception {
 
         throw new RuntimeException("RuntimeException 발생");
 
     }
 
-    @DisplayName("NullPointerException 오류 예상 테스트")
+
     @Test(expected= NullPointerException.class)
+    @DisplayName("NullPointerException 오류 예상 테스트")
     public void JunitAnnotationTest4() throws Exception {
 
         String str = null;
@@ -41,30 +44,29 @@ public class AnnotationTest {
 
     }
 
-
+    /******************************************/
     /*******  @BeforeClass 와 @Before  *******/
 
-    @BeforeClass
-    public static void beforeClassTest(){
-        System.out.println("@BeforeClass");
-    }
-
-    @AfterClass
-    public static void afterClassTest(){
-        System.out.println("@AfterClass");
-    }
+//    @BeforeClass
+//    public static void beforeClassTest(){
+//        System.out.println("@BeforeClass");
+//    }
+//
+//    @AfterClass
+//    public static void afterClassTest(){
+//        System.out.println("@AfterClass");
+//    }
 
     @Before
     public void beforeTest(){
-        System.out.println("테스트 메소드 시작 전 @Before");
+        System.out.println("각 메소드 시작 전 @Before");
 
     }
 
     @After
     public void afterTest(){
-        System.out.println("테스트 메소드 시작 후 @After");
+        System.out.println("각 메소드 시작 후 @After");
     }
-
 
 
 }
--- testJUnit/src/test/java/com/example/testjunit/AssertionTest.java
+++ testJUnit/src/test/java/com/example/testjunit/AssertionTest.java
@@ -4,6 +4,7 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 
@@ -17,120 +18,55 @@ import static org.assertj.core.api.Assertions.*;
 
 public class AssertionTest {
 
-    @DisplayName("로또 번호 갯수 테스트 1")
+
     @Test
-    void lottoNumberSizeTest(){
+    @DisplayName("assertTrue() 테스트")
+    public void testAssertTrue(){
 
-        System.out.println("로또 번호 갯수 테스트 1");
-        //given
-        final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        final int price = 1000;
+        int num = 3;
 
-        //when
-        final List<Integer> lottoNumber = lottoNumberGenerator.generate(price);
+//        Assert.assertTrue(num == 3);
+        assertTrue((num < 2), "false 입니다.");
 
-        //then
-//        assertEquals(6, lottoNumber.size());
-//        assertThat(lottoNumber.size()).isEqualTo(1);
-
-        int lottoSize = lottoNumber.size();
-//        assertTrue(isRightLottoSize(lottoSize, 9), "lotto 갯수 잘못 출력됨");
-        assertEquals(2,2, "두 개의 값은 다르다.");
-
-    }
-
-    @DisplayName("로또 번호 갯수 테스트 2")
-    @Test
-    void lottoNumberSizeTest2(){
-        System.out.println("로또 번호 갯수 테스트 2");
-
-        //given
-        final LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        final int price = 1000;
-
-        //when
-        final List<Integer> lottoNumber = lottoNumberGenerator.generate(price);
-
-        //then
-//        assertEquals(6, lottoNumber.size());
-//        assertThat(lottoNumber.size()).isEqualTo(1);
-
-        int lottoSize2 = lottoNumber.size();
-        assertTrue(isRightLottoSize(lottoSize2), "lotto 갯수 잘못 출력됨");
-
-    }
-
-    private boolean isRightLottoSize(int num1) {
-
-        if(num1 == 6){
-            return true;
-
-        } else {
-            return false;
-        }
-    }
-
-    // *** @BeforeEach와 @AfterEach
-    @BeforeEach
-    public void beforeEach() {
-        System.out.println("beforeEach");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        System.out.println("afterEach");
-    }
-    
-    @BeforeAll
-    public static void beforeAll() {
-        System.out.println("*** beforeAll **"); 
-    }
-    
-    @AfterAll
-    public static void afterAll() {
-        System.out.println("*** afterAll *** ");
-    }
-
-    @Disabled("Not implemented yet")
-    @Test
-    public void testDisabled(){
-        System.out.println("@Disabled 테스트");
     }
 
     @Test
-    @DisplayName("assertAll 테스트")
-    public void testAssertAll(){
-        int[] numbers = {1,2,3,4,5,6,7,8,9};
-        assertAll("numbers",
-                () -> assertEquals(numbers[0], 8),
-                () -> assertEquals(numbers[1], 5),
-                ()  -> assertEquals(numbers[2], 6)
-                );
+    @DisplayName("assertEquals() 테스트")
+    public void testAssertEquals(){
 
+        int num = 1;
+        assertEquals(12, num);
     }
+
 
     @Test
     @DisplayName("assertThat() 테스트")
     public void testAssertThat(){
 
-        String expected = "Hello, World";
         String actual = "hello world23433";
-//
-        assertThat(actual, is(equalTo(expected)));
+
+        assertThat(actual)
+                .isEqualTo("hello world23433")
+                .isNotNull()
+                .startsWith("hello")
+                .endsWith("1");
 
 
+
+        /**
         // assertJ 라이브러리의 전통적인 방식
-//        assertThat(actual)
-//                .isAlphabetic()
-//                .isEqualTo(expected)
-//                .isASCII()
-//                .isLowerCase()
-//                .isNotEmpty();
+        assertThat(actual)
+                .isAlphabetic()
+                .isEqualTo(expected)
+                .isASCII()
+                .isLowerCase()
+                .isNotEmpty();
 
-        // 코드 실행 시 오류 발생하면 테스트 멈추게 된다.
 
         // Hamcrest 방식
         assertThat(actual, equalTo(expected));
+
+         **/
     }
 
     @Test
@@ -142,6 +78,7 @@ public class AssertionTest {
 //        assertThat(expectedNum)
 //                .isBetween(12, 1280);
     }
+
 
     // 라이브러리 AssertJ vs Hamcrest
     @Test
@@ -221,6 +158,18 @@ public class AssertionTest {
         assertThat(number, is(greaterThan(0)));
 
     }
-    
+
+//    @Test
+//    @DisplayName("assertAll 테스트")
+//    public void testAssertAll(){
+//        int[] numbers = {1,2,3,4,5,6,7,8,9};
+//        assertAll("numbers",
+//                () -> assertEquals(numbers[0], 8),
+//                () -> assertEquals(numbers[1], 5),
+//                ()  -> assertEquals(numbers[2], 6)
+//                );
+//
+//    }
+
 
 }
